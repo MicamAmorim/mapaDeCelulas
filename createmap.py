e05d3a860f1c48d21bc75ab477d12ec65c5229fc
MicamAmorim <96069299+MicamAmorim@users.noreply.github.com>
2024-01-08 11:02:54 -0300

    Add files via upload

---
+++ createmap.py
@@ -0,0 +1,54 @@
+# pip install pandas geopy folium geocoder ipywidgets
+import pandas as pd
+import geopy
+from geopy.geocoders import Nominatim
+import folium
+from ipywidgets import HTML
+import geocoder
+from folium import IFrame
+import base64
+
+def createMap(df, printme = False):
+  # Inicializa o geocodificador
+  geolocator = Nominatim(user_agent="geoapiExercises")
+  vep_lat = -14.802158819744324
+  vep_long = -39.277382080808714
+
+  # Função para converter endereço em latitude e longitude
+  def geocode_address(address):
+      try:
+          location = geolocator.geocode(address)
+          return location.latitude, location.longitude
+      except:
+          print('Not Found')
+          return None, None
+
+  def update_geocode(row):
+    if pd.isnull(row['Latitude']) or pd.isnull(row['Longitude']):
+        lat, lng = geocode_address(row['Endereco'])
+        return lat, lng
+    else:
+        return row['Latitude'], row['Longitude']
+
+  # Aplica a função de atualização ao DataFrame
+  df['Latitude'], df['Longitude'] = zip(*df.apply(update_geocode, axis=1))
+
+  # Cria um mapa
+  m = folium.Map(location=[vep_lat, vep_long], zoom_start=15)
+
+  # Adiciona um marcador para cada localização
+  for _, row in df.iterrows():
+      if pd.notnull(row['Latitude']) and pd.notnull(row['Longitude']):
+          tooltip = f"{row['Nome']}\n{row['Telefone']}\n{row['Endereco']}\n{row['Dia']} - {row['Horario']}"         
+
+          folium.Marker([row['Latitude'], row['Longitude']], popup=tooltip, tooltip=tooltip).add_to(m)
+
+  # Adiciona um marcador para a localização do usuário
+  if printme:
+    # Obter localização atual do usuário
+    user_location = geocoder.ip('me')
+    user_lat, user_lng = user_location.latlng
+    folium.Marker([user_lat, user_lng], popup='Sua Localização', icon=folium.Icon(color='red')).add_to(m)
+
+  # Exibe o mapa
+  return m
---
+++ main.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd
+from createmap import createMap  # Supondo que a função createMap esteja em um script chamado 'your_script.py'
+import streamlit.components.v1 as components
+
+# Configurações do tema
+st.set_page_config(
+    page_title="Visualizador de Mapas",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# Esta é uma aplicação para visualizar mapas."
+    }
+)
+
+# Define um estilo CSS para o título
+title_html = """
+    <style>
+        .title {
+            font-family: 'Comic Sans MS', 'Comic Sans', cursive; /* Escolha a fonte */
+            font-style: italic; /* Estilo itálico */
+        }
+    </style>
+    <div class='title'>Encontre a célula mais próxima de sua casa</div>
+"""
+
+# Usa st.sidebar.markdown para renderizar o HTML com o estilo CSS
+st.sidebar.markdown(title_html, unsafe_allow_html=True)
+
+uploaded_file = "Database/dados.csv"
+
+if uploaded_file:
+    # Carrega os dados para um DataFrame
+    df = pd.read_csv(uploaded_file)
+    for endereco in df['Endereco']:
+        st.sidebar.write(f"📍 {endereco}") 
+    # Chama a função para criar o mapa
+    mapa = createMap(df)
+
+    # Mostra o mapa na página
+    #st.pydeck_chart(mapa)
+    mapa.save('mapa.html')
+    with open('mapa.html', 'r', encoding='utf-8') as f:
+        html_string = f.read()
+        html_string = html_string.replace('<head>', '<head><style>html, body {width: 100%; height: 100%; margin: 0; padding: 0;} #map {width: 100% !important; height: 100% !important;}</style>')
+
+        # Exibe o mapa no Streamlit
+        components.html(html_string, height=500, scrolling=True)
+
+
+else:
+    st.sidebar.warning("Por favor, carregue um arquivo CSV.")
